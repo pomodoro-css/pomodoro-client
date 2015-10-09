f79666f9e0929aa03ff7e6efa040cf562d3cf88f
Manuel <manuel.mueller.meier@gmail.com>
2015-10-09 17:24:14 +0200

    final RegistrationService

--- pomodoro-client/src/main/java/ch/css/pomodoro/client/PomodoreSystemUtils.java
+++ pomodoro-client/src/main/java/ch/css/pomodoro/client/PomodoreSystemUtils.java
@@ -3,9 +3,11 @@ package ch.css.pomodoro.client;
 public class PomodoreSystemUtils {
 	
 	private static String host;
+	private static String beginPath;
 	
 	private PomodoreSystemUtils(){
 		host = "stub";
+		beginPath = "/users/";
 	}
 	
 	public static void setHost(String server){
@@ -14,6 +16,18 @@ public class PomodoreSystemUtils {
 
 	public static String getHost() {
 		return host;
+	}
+
+	public static String getBeginPath() {
+		return beginPath;
+	}
+
+	public static void setBeginPath(String beginPath) {
+		PomodoreSystemUtils.beginPath = beginPath;
+	}
+	
+	public static String getBasisUrl(){
+		return host + beginPath;
 	}
 
 
--- pomodoro-client/src/main/java/ch/css/pomodoro/client/RegistrationService.java
+++ pomodoro-client/src/main/java/ch/css/pomodoro/client/RegistrationService.java
@@ -1,16 +1,38 @@
 package ch.css.pomodoro.client;
 
+import javax.ws.rs.core.MediaType;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
 public class RegistrationService {
 	private static Logger logger = LoggerFactory.getLogger(RegistrationService.class);
-	
+
 	public boolean callRegistrationService(String pNummer, String name, String group) {
 		// handle 404 Error
+		String basisURL = PomodoreSystemUtils.getBasisUrl();
+		logger.info(String.format("BasisURL for Registration Service: %s", basisURL));
+		StringBuilder callUrl = new StringBuilder(basisURL);
+		callUrl.append("?nr="+pNummer);
+		callUrl.append("&name="+name);
+		if (group != null) {
+			callUrl.append("&group="+group);
+		}
 		
+		logger.info(String.format("URL for Registration Service: %s", callUrl.toString()));
+
+		Client client = Client.create();
+		WebResource webResource = client.resource(callUrl.toString());
+
+		ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON_TYPE)
+				.post(ClientResponse.class);
+
 		return true;
-		
+
 	}
 
 }
