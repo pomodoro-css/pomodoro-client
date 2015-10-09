82282e18bb22fe9b27610507bc5e1f6261b7ef64
rrao88 <rahulrao2001@hotmail.com>
2015-10-09 14:44:30 +0200

    Folder order and started registration ui

---
+++ pomodoro-client/src/main/java/ch/css/pomodoro/client/RegistrationUI.java
@@ -0,0 +1,15 @@
+package ch.css.pomodoro.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RegistrationUI {
+
+	private static Logger logger = LoggerFactory.getLogger(RegistrationUI.class);
+
+	public void registerClient() {
+		logger.info("Starting client registration!");
+
+	}
+
+}
