9be901d188b8667e3780c52de0446d483c765081
Manuel <manuel.mueller.meier@gmail.com>
2015-10-09 15:32:56 +0200

    Gruppe hinzugefügt, Feldvalidierung

--- pomodoro-client/src/main/java/ch/css/pomodoro/client/RegistrationUI.java
+++ pomodoro-client/src/main/java/ch/css/pomodoro/client/RegistrationUI.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,10 +31,11 @@ public class RegistrationUI {
 
 	private void createRegistrationUI() {
 		JFrame frame = new JFrame("Registration");
+		frame.setIconImage(IconFactory.createAppIconBig().getImage());
 		// Center on Screen
 		frame.pack();
 		frame.setLocationRelativeTo(null);
-		frame.setSize(300, 150);
+		frame.setSize(300, 180);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -52,6 +54,7 @@ public class RegistrationUI {
 		panel.add(userPnrLabel);
 
 		JTextField userPnrText = new JTextField();
+		userPnrText.setDocument(new JTextFieldLimit(6));
 		userPnrText.setBounds(100, 10, 160, 25);
 		panel.add(userPnrText);
 
@@ -60,11 +63,21 @@ public class RegistrationUI {
 		panel.add(userNameLabel);
 
 		JTextField userNameTextField = new JTextField();
+		userNameTextField.setDocument(new JTextFieldLimit(50));
 		userNameTextField.setBounds(100, 40, 160, 25);
 		panel.add(userNameTextField);
 
+		JLabel gruppeNameLabel = new JLabel("Gruppe");
+		gruppeNameLabel.setBounds(10, 70, 80, 25);
+		panel.add(gruppeNameLabel);
+
+		JTextField gruppeNameLabelField = new JTextField();
+		gruppeNameLabelField.setDocument(new JTextFieldLimit(50));
+		gruppeNameLabelField.setBounds(100, 70, 160, 25);
+		panel.add(gruppeNameLabelField);
+
 		JButton registerButton = new JButton("Register");
-		registerButton.setBounds(10, 80, 80, 25);
+		registerButton.setBounds(10, 110, 80, 25);
 		panel.add(registerButton);
 	}
 }
