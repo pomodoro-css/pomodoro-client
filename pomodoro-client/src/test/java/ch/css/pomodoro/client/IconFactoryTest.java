6b0390c1dbb5b2b789adc5af1a924c2dd1c9ccaf
rrao88 <rahulrao2001@hotmail.com>
2015-10-09 14:31:50 +0200

    created test folder

---
+++ pomodoro-client/src/test/java/ch/css/pomodoro/client/IconFactoryTest.java
@@ -0,0 +1,13 @@
+package ch.css.pomodoro.client;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IconFactoryTest {
+
+	@Test
+	public void myTest() {
+		Assert.assertEquals(true, true);
+	}
+
+}
