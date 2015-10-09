a346402d0eb41739c1f4f40fe8f2fd21a5854492
sascha <sascha@debian>
2015-10-09 22:38:53 +0200

    launcher improved

--- pomodoro-client/src/main/java/ch/css/pomodoro/client/Main.java
+++ pomodoro-client/src/main/java/ch/css/pomodoro/client/Main.java
@@ -11,7 +11,8 @@ import org.slf4j.LoggerFactory;
 public class Main {
 	private static Logger logger = LoggerFactory.getLogger(Main.class);
 
-	private static final String DEFAULT_HOST = "http://lnx54064.css.ch:12345";
+	private static final String DEFAULT_HOST = "http://localhost:8080";
+	//private static final String DEFAULT_HOST = "http://lnx54064.css.ch:12345";
 
 	{
 		Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
