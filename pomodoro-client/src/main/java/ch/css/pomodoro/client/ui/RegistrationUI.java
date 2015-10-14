9156d17b040567a098c6a2b51e05deaed9b31999
rrao88 <rahulrao2001@hotmail.com>
2015-10-14 21:10:14 +0200

    Added cancel button to UI and stopped (x) from completetly shutting down application

--- pomodoro-client/src/main/java/ch/css/pomodoro/client/ui/RegistrationUI.java
+++ pomodoro-client/src/main/java/ch/css/pomodoro/client/ui/RegistrationUI.java
@@ -1,5 +1,6 @@
 package ch.css.pomodoro.client.ui;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -23,6 +24,7 @@ public class RegistrationUI extends JFrame {
 	private static JTextField userNameText;
 	private static JTextField gruppeNameText;
 	private static JButton registerButton;
+	private static JButton cancelButton;
 
 	public RegistrationUI() {
 		logger.info("Starting client registration!");
@@ -34,9 +36,9 @@ public class RegistrationUI extends JFrame {
 		// Center on Screen
 		this.pack();
 		this.setLocationRelativeTo(null);
-		this.setSize(300, 170);
+		this.setSize(280, 170);
 		this.setResizable(false);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
 		setupContent();
 		this.setVisible(true);
@@ -48,36 +50,52 @@ public class RegistrationUI extends JFrame {
 		panel.setLayout(null);
 
 		JLabel userPnrLabel = new JLabel("P-Nummer *:");
-		userPnrLabel.setBounds(10, 10, 85, 25);
+		userPnrLabel.setBounds(10, 10, 90, 25);
+		Font pnrFont = new Font(userPnrLabel.getFont().getName(), Font.BOLD, userPnrLabel.getFont()
+				.getSize());
+		userPnrLabel.setFont(pnrFont);
 		panel.add(userPnrLabel);
 
 		userPnrText = new JTextField();
 		userPnrText.setDocument(new JTextFieldLimit(6));
-		userPnrText.setBounds(100, 10, 160, 25);
+		userPnrText.setBounds(110, 10, 160, 25);
 		panel.add(userPnrText);
 
 		JLabel userNameLabel = new JLabel("Name *:");
-		userNameLabel.setBounds(10, 40, 85, 25);
+		userNameLabel.setBounds(10, 40, 90, 25);
+		Font nameFont = new Font(userNameLabel.getFont().getName(), Font.BOLD, userNameLabel
+				.getFont().getSize());
+		userNameLabel.setFont(nameFont);
 		panel.add(userNameLabel);
 
 		userNameText = new JTextField();
 		userNameText.setDocument(new JTextFieldLimit(30));
-		userNameText.setBounds(100, 40, 160, 25);
+		userNameText.setBounds(110, 40, 160, 25);
 		panel.add(userNameText);
 
 		JLabel gruppeNameLabel = new JLabel("Gruppe:");
-		gruppeNameLabel.setBounds(10, 70, 85, 25);
+		gruppeNameLabel.setBounds(10, 70, 90, 25);
 		panel.add(gruppeNameLabel);
 
 		gruppeNameText = new JTextField();
 		gruppeNameText.setDocument(new JTextFieldLimit(50));
-		gruppeNameText.setBounds(100, 70, 160, 25);
+		gruppeNameText.setBounds(110, 70, 160, 25);
 		panel.add(gruppeNameText);
 
+		// registerButton = new JButton("Register");
+		// registerButton.setBounds(185, 110, 80, 25);
+		// registerButton.addActionListener(new RegistrationActionListener());
+		// panel.add(registerButton);
+
 		registerButton = new JButton("Register");
-		registerButton.setBounds(185, 110, 80, 25);
+		registerButton.setBounds(10, 110, 100, 25);
 		registerButton.addActionListener(new RegistrationActionListener());
 		panel.add(registerButton);
+
+		cancelButton = new JButton("Cancel");
+		cancelButton.setBounds(170, 110, 100, 25);
+		cancelButton.addActionListener(new CancelActionListener());
+		panel.add(cancelButton);
 
 		this.add(panel);
 
@@ -96,6 +114,15 @@ public class RegistrationUI extends JFrame {
 				regHandler.registerClient();
 			}
 		}
+	}
+
+	class CancelActionListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			dispose();
+		}
+
 	}
 
 }
