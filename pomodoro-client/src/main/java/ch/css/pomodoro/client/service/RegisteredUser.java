bbe55acd1085c997569a5766c6ef86af9019f0f2
Manuel <manuel.mueller.meier@gmail.com>
2015-10-15 10:33:28 +0200

    Status call

---
+++ pomodoro-client/src/main/java/ch/css/pomodoro/client/service/RegisteredUser.java
@@ -0,0 +1,71 @@
+package ch.css.pomodoro.client.service;
+
+public class RegisteredUser {
+	private String nr;
+
+	private String name;
+	private String group;
+	private UserState state;
+	private int remainingTime;
+	private int tomatoTime;
+
+	private String startTime;
+
+	public String getNr() {
+		return nr;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGroup() {
+		return group;
+	}
+
+	public void setGroup(String group) {
+		this.group = group;
+	}
+
+	public UserState getState() {
+		return state;
+	}
+
+	public void setState(UserState state) {
+		this.state = state;
+	}
+
+	public int getRemainingTime() {
+		return remainingTime;
+	}
+
+	public void setRemainingTime(int remainingTime) {
+		this.remainingTime = remainingTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public boolean hasStartTime() {
+		return startTime != null;
+	}
+
+	public void setTomatoTime(int time) {
+		this.tomatoTime = time;
+	}
+
+	public int getTomatoTime() {
+		return tomatoTime;
+	}
+
+
+}
---
+++ pomodoro-client/src/main/java/ch/css/pomodoro/client/service/StatusRegisteredUsersService.java
@@ -0,0 +1,53 @@
+package ch.css.pomodoro.client.service;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import ch.css.pomodoro.client.utility.PomodoreSystemUtils;
+
+public class StatusRegisteredUsersService {
+
+	private static Logger logger = LoggerFactory.getLogger(StatusRegisteredUsersService.class);
+
+	public List<RegisteredUser> callStatusRegisteredUsers() {
+		String basisURL = PomodoreSystemUtils.getBasisUrl();
+		StringBuilder callUrl = new StringBuilder(basisURL);
+
+		logger.info(String.format("URL for Registeres Users Service: %s", callUrl.toString()));
+
+		Client client = Client.create();
+		WebResource webResource = client.resource(callUrl.toString());
+
+		String responseSring = webResource.accept(MediaType.APPLICATION_JSON)
+				.type(MediaType.APPLICATION_JSON_TYPE).get(String.class);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			return mapper.readValue(responseSring, new TypeReference<List<RegisteredUser>>(){});
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+}
