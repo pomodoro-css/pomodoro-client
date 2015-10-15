c5998ed741fb3cd9b653bb8a053c0738aeacf478
rrao88 <rahulrao2001@hotmail.com>
2015-10-15 17:59:15 +0200

    Next bugfix

--- pomodoro-client/src/main/java/ch/css/pomodoro/client/service/StartTimerService.java
+++ pomodoro-client/src/main/java/ch/css/pomodoro/client/service/StartTimerService.java
@@ -1,35 +1,38 @@
 package ch.css.pomodoro.client.service;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import javax.ws.rs.core.MediaType;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.css.pomodoro.client.utility.PomodoreSystemUtils;
+import ch.css.pomodoro.client.utility.UserInfo;
+
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
-import ch.css.pomodoro.client.utility.PomodoreSystemUtils;
-import ch.css.pomodoro.client.utility.UserInfo;
-
 public class StartTimerService {
 	private static Logger logger = LoggerFactory.getLogger(StartTimerService.class);
 
-	public boolean callStartTimerService() {
+	public boolean callStartTimerService() throws UnsupportedEncodingException {
 		// handle 404 Error
 		String basisURL = PomodoreSystemUtils.getBasisUrl();
 		StringBuilder callUrl = new StringBuilder(basisURL);
-		callUrl.append(UserInfo.getPNummer());
+		callUrl.append(URLEncoder.encode(UserInfo.getPNummer(), "UTF-8"));
 		callUrl.append("/start");
-		
+
 		logger.info(String.format("URL for StartTimer Service: %s", callUrl.toString()));
 
 		Client client = Client.create();
 		WebResource webResource = client.resource(callUrl.toString());
 
-		ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON_TYPE)
-				.put(ClientResponse.class);
-		
+		ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON)
+				.type(MediaType.APPLICATION_JSON_TYPE).put(ClientResponse.class);
+
 		if (response.getStatus() != 200) {
 			return false;
 		}
